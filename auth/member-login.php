31bf978333e8882e79ed61f8f7fb69604d8b2718
Gushantheekshana <iamtheekshana@gmail.com>
2026-09-17 22:22:01 +0530

    fixed member-login.php

--- auth/member-login.php
+++ auth/member-login.php
@@ -2,4 +2,4 @@
 $loginRole = 'MEMBER';
 $loginTitle = 'Member sign in';
 $loginDescription = 'Manage your membership, classes, attendance, and messages.';
-//require __DIR__ . '/role-login.php';
+require __DIR__ . '/role-login.php';
